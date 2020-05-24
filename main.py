2d0b4964efbdef31d0a36d9d7f18c98dcd5c0455
Hassan Askary <imhassanaskary@gmail.com>
2020-05-24 15:24:16 +0500

    added selection interface

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torchvision
 from torchvision import transforms, models
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import argparse
 import random
 
@@ -36,18 +36,31 @@ def get_prediction(img):
 
         classes = []
         masks = []
+        boxes = []
 
         scores = list(prediction['scores'].detach().numpy()) # converting to numpy array then to a list
         classes_list = list(prediction['labels'].detach().numpy()) # indices of detected labels
         masks_list = list((prediction['masks']>0.5).squeeze().detach().numpy())
+        boxes_list = list(prediction['boxes'].detach().numpy())
 
         scores = [scores.index(i) for i in scores if i > 0.5] # indices of matches that have higher than 0.5 confidence
 
         for i in scores:
             classes.append(LABEL_NAMES[classes_list[i]])
             masks.append(masks_list[i])
+            boxes.append([(boxes_list[i][0], boxes_list[i][1]), (boxes_list[i][2], boxes_list[i][3])])
 
-        return classes, masks
+        return classes, masks, boxes
+
+def get_choice_img(primary_img, classes, boxes):
+    choice_img = primary_img.copy()
+    
+    draw = ImageDraw.Draw(choice_img)
+    for i in range(len(classes)):
+        draw.rectangle((boxes[i][0], boxes[i][1]))
+        draw.text(boxes[i][0], f"{classes[i]} {i}", fill='red', font=ImageFont.truetype("opensans.ttf", 20))
+    
+    return choice_img
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Semantic Image Manipulator')
@@ -59,7 +72,12 @@ if __name__ == '__main__':
     primary_img = Image.open(opt.primary_image).convert('RGB')
     secondary_img = Image.open(opt.secondary_image).convert('RGB')
 
-    classes, masks = get_prediction(primary_img)
+    classes, masks, boxes = get_prediction(primary_img)
+
+    if secondary_img.width < primary_img.width or secondary_img.height < primary_img.height:
+        secondary_img = secondary_img.resize(
+            (int(primary_img.width * 1.2), int(primary_img.height * 1.2))
+        )
 
     secondary_img = secondary_img.crop((0, 0, masks[0].shape[1], masks[0].shape[0]))
 
@@ -68,17 +86,28 @@ if __name__ == '__main__':
     choice = set(classes)
     choice.add('background')
 
+    choice_img = get_choice_img(primary_img, classes, boxes)
+    plt.imshow(choice_img)
+    plt.show()
+
     print("Classes found: ", choice)
-    input_class = str(input("Enter a class: "))
+    selection = str(input("Enter class names or instance numbers (comma seperated): "))
+    selection = selection.split(',')
+    selection = [x.strip() for x in selection]
+    selection = [int(x) if x.isdigit() else x for x in selection]
 
     mask = np.zeros_like(masks[0], dtype=np.uint8)
 
     for i, c in enumerate(classes):
-        if c == input_class or input_class == 'background':
+        if c in selection or i in selection:
             mask += (masks[i].astype(np.uint8)) * 255
 
-    if input_class == 'background':
-        mask = (np.where(mask == 255, 0, 255)).astype(np.uint8)
+    if 'background' in selection:
+        background = np.zeros_like(masks[0], dtype=np.uint8)
+        for m in masks:
+            background += (m.astype(np.uint8)) * 255
+        background = (np.where(background == 255, 0, 255)).astype(np.uint8)
+        mask += background
 
     mask = Image.fromarray(mask)
 
